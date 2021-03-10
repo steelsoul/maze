610ccc8664ab77d5b51d4fe5e445c402aa2c9494
Alexander Pushkar <alexanderpushkar86@gmail.com>
2021-03-10 08:30:20 +0100

    Remove thread for maze generation
    It looks that threads are not supported for html5 in Godot.

--- code/Main.gd
+++ code/Main.gd
@@ -9,22 +9,17 @@ var maze_generator: MazeGenerator = null
 var quest_holder: QuestHolder = null
 
 onready var configuration_ = $CanvasLayer/Configuration
-var thread: Thread = null
 
 func _on_Configuration_configuration_done():
 	configuration_.hide()
 	maze_generator = MazeGenerator.new(configuration_.get_dim())
 	maze_generator.connect("generation_done", self, "_on_Generation_done")
-	thread = Thread.new()
-	$CanvasLayer/InProgressLabel.show()
-	thread.start(self, "generator_thread", [configuration_, maze_generator])
 
-func generator_thread(data):
-	var config = 	data[0]
-	var gen	=		data[1]
-	match config.get_algorythm():
-		"kruskal": 	gen.generate_kruskal()
-		"prima": 	gen.generate_prima()
+	$CanvasLayer/InProgressLabel.show()
+	
+	match configuration_.get_algorythm():
+		"kruskal": 	maze_generator.generate_kruskal()
+		"prima": 	maze_generator.generate_prima()
 
 func _on_Generation_done():
 	maze_generator.disconnect("generation_done", self, "_on_Generation_done")
@@ -32,10 +27,6 @@ func _on_Generation_done():
 
 func _on_Maze_on_game_finished():
 	configuration_.show()
-
-func _exit_tree():
-	if thread != null:
-		thread.wait_to_finish()
 
 func _on_Maze_game_over():
 	$Maze.hide()
@@ -45,9 +36,6 @@ func _on_Maze_game_over():
 func _on_Timer_timeout():
 	quest_holder = QuestHolder.new(maze_generator.get_maze(), configuration_.is_quest_mode())
 	$Maze.setup_maze(maze_generator.get_maze(), quest_holder)
-	thread.wait_to_finish()
-	thread = null
-	maze_generator = null
 	$CanvasLayer/InProgressLabel.hide()
 	var start_point = Vector2.ZERO
 	var goal_point = configuration_.get_dim() - Vector2(1,1)
